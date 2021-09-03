a5d87e8b38a5eff48e0939d3899c75465795d5de
harukixzh <90026722+harukixzh@users.noreply.github.com>
2021-09-03 02:15:30 -0600

    Update script.js

--- mini-player-master/scripts/script.js
+++ mini-player-master/scripts/script.js
@@ -20,7 +20,7 @@ new Vue({
           name: "Demasiadas Mujeres",
           artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-         
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/01 - C. Tangana - Demasiadas Mujeres.mp3",
           url: "https://www.youtube.com/watch?v=ZlFri4ez_lE",
           favorited: true
         },
@@ -28,7 +28,7 @@ new Vue({
           name: "Tú Me Dejaste De Querer",
           artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-  
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/02 - C. Tangana, Niño de Elche, La Húngara - Tú Me Dejaste De Querer.mp3", 
           url: "https://www.youtube.com/watch?v=ltmO9XQVdSg",
           favorited: true
         },
@@ -36,7 +36,7 @@ new Vue({
           name: "Comerte Entera",
           artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/03 - C. Tangana, Toquinho - Comerte Entera.mp3",
           url: "https://www.youtube.com/watch?v=3xlExHPyqM0",
           favorited: true
         },
@@ -44,7 +44,7 @@ new Vue({
           name: "Nunca Estoy",
           artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/04 - C. Tangana - Nunca Estoy.mp3",
           url: "https://www.youtube.com/watch?v=U6phuhL1YbY",
           favorited: true
         },
@@ -52,7 +52,7 @@ new Vue({
           name: "Párteme La Cara",
           artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/05 - C. Tangana, Ed Maverick - Párteme La Cara.mp3",
           url: "https://www.youtube.com/watch?v=gcS2H3p-YOg",
           favorited: true
         },
@@ -60,7 +60,7 @@ new Vue({
           name: "Ingobernable",
           artist: "C.Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/06 - C. Tangana, Gipsy Kings, Nicolás Reyes, Tonino Baliardo - Ingobernable.mp3",
           url: "https://www.youtube.com/watch?v=uV0r4a2QVkQ",
           favorited: true
         },
@@ -68,7 +68,7 @@ new Vue({
           name: "Nominao",
           artist: "C. Tangana",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/07 - C. Tangana, Jorge Drexler - Nominao.mp3",
           url: "https://www.youtube.com/watch?v=wAZZ9Xe3HKA",
           favorited: true
         },
@@ -76,18 +76,59 @@ new Vue({
           name: "Un Veneno - G-Mix",
           artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/08 - C. Tangana, José Feliciano, Niño de Elche - Un Veneno - G-Mix.mp3",
           url: "https://www.youtube.com/watch?v=h0Tb9VtVzVE",
           favorited: true
         },
         {
-          name: "Rag'n'Bone Man",
-          artist: "Human",
+          name: "Te Olvidaste",
+          artist: "C. Tangana",
           cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
-
-          url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
-          favorited: false
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/09 - C. Tangana, Omar Apollo - Te Olvidaste.mp3",
+          url: "https://www.youtube.com/watch?v=cZ9NrVKVywk",
+          favorited: true
+        },
+        {
+          name: "Muriendo De Envidia",
+          artist: "C. Tangana",
+          cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/10 - C. Tangana, Eliades Ochoa - Muriendo De Envidia.mp3",
+          url: "https://www.youtube.com/watch?v=HfkjnAv_uRE",
+          favorited: true
+        },
+        {
+          name: "CAMBIA!",
+          artist: "C. Tangana",
+          cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/11 - C. Tangana, Carin Leon, Adriel Favela - CAMBIA!.mp3",
+          url: "https://www.youtube.com/watch?v=Bs-jN1EV0q0",
+          favorited: true
+        },
+        {
+          name: "Cuándo Olvidaré",
+          artist: "C. Tangana",
+          cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/12 - C. Tangana, Pepe Blanco - Cuándo Olvidaré.mp3",
+          url: "https://www.youtube.com/watch?v=A6hDkJEbcuE",
+          favorited: true
+        },
+        {
+          name: "Los Tontos",
+          artist: "C. Tangana",
+          cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/13 - C. Tangana, Kiko Veneno - Los Tontos.mp3",
+          url: "https://www.youtube.com/watch?v=vjWyRfnR5CQ",
+          favorited: true
+        },
+        {
+          name: "Hong Kong",
+          artist: "C. Tangana",
+          cover: "https://aws.traveler.es/prod/designs/v1/assets/290x290/203216.jpg",
+          source: "https://raw.githubusercontent.com/haruki/C. Tangana/mini-player-master/mp3/14 - C. Tangana, Andrés Calamaro - Hong Kong.mp3",
+          url: "https://www.youtube.com/watch?v=XLLkSBLqlvQ",
+          favorited: true
         }
+        
       ],
       currentTrack: null,
       currentTrackIndex: 0,
@@ -104,7 +145,29 @@ new Vue({
         this.isTimerPlaying = false;
       }
     },
-    
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
     updateBar(x) {
       let progress = this.$refs.progress;
       let maxduration = this.audio.duration;
